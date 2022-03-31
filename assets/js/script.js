a1adc351221261b941eda61051ea519eeafb64cf
CarlosMongeJr <carlosmongejr@gmail.com>
2022-03-30 20:52:56 -0400

    added styles and main text

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const typed = new Typed('.animate', {
+    strings: ['Coder', 'Programmer', 'Developer', 'Freelancer'],
+    typeSpeed: 75,
+    backSpeed: 75,
+    loop: true,
+    backDelay: 1500,
+    startDelay: 300,
+    cursorChar:'|'
+});
